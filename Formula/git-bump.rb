9a5420fea3d474d5628c7a2715f93584f884e19e
Tim Hårek Andreassen <tim@harek.no>
2025-06-01 00:29:28 +0200

    git-bump v0.1.1

--- Formula/git-bump.rb
+++ Formula/git-bump.rb
@@ -1,8 +1,8 @@
 class GitBump < Formula
   desc "Easily tag new versions (semver) with git."
   homepage "https://git.sr.ht/~timharek/git-bump"
-  url "https://git.sr.ht/~timharek/git-bump/archive/v0.1.0.tar.gz"
-  sha256 "7c900de5c520490e5c95dd0bd42975298d1ffa8b626b35ae88376723c0d0d5bc"
+  url "https://git.sr.ht/~timharek/git-bump/archive/v0.1.1.tar.gz"
+  sha256 "6ff6aeeac8d262d858b5d7be3878d3dedc7f9892fb2bb3ab38bbc2400c9c0eda"
   license "GPLv3" 
 
   depends_on "go" => :build
