9d948b6c180a47457e5cbed2485a93c3e5d5d6a7
Tim Hårek Andreassen <tim@harek.no>
2025-06-17 22:44:50 +0200

    yr v1.0.0

--- Formula/yr.rb
+++ Formula/yr.rb
@@ -1,8 +1,8 @@
 class Yr < Formula
   desc "CLI tool for checking weather"
   homepage "https://git.sr.ht/~timharek/yr"
-  url "https://git.sr.ht/~timharek/yr/archive/v0.0.11.tar.gz"
-  sha256 "be83d0f46aae37ebb6a508091f0a4282d97f03eb763a844087a8a84ec21393e2"
+  url "https://git.sr.ht/~timharek/yr/archive/v1.0.0.tar.gz"
+  sha256 "bc8980f06abc23b80fa2e8a8adcfbeeb3fa98b35d4ae883540c9e3c6cb627cba"
   license "GPLv3" 
 
   depends_on "go" => :build
