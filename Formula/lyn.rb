d48f9be777455fd05d47385444098b3400462a37
Tim Hårek Andreassen <tim@harek.no>
2026-03-22 10:48:52 +0100

    lyn v1.1.0

--- Formula/lyn.rb
+++ Formula/lyn.rb
@@ -1,8 +1,8 @@
 class Lyn < Formula
   desc "CLI tool for checking weather"
   homepage "https://git.sr.ht/~timharek/lyn"
-  url "https://git.sr.ht/~timharek/lyn/archive/v1.0.0.tar.gz"
-  sha256 "eb26b3eac8db0a616daa4b1ac93bfcc2fc9451293b09ad2263f4f6db8f2f3220"
+  url "https://git.sr.ht/~timharek/lyn/archive/v1.1.0.tar.gz"
+  sha256 "3595146c6769657f85e339943ce60a98f66a822012e0cd5a980a8a268a650fff"
   license "GPLv3" 
 
   depends_on "go" => :build
